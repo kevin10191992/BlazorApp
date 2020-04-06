b8c62a7b6c41bf40590f0bad47059002cd74422e
kperez <kevin10191992@hotmail.com>
2020-04-06 17:52:48 -0500

    Agregar archivos de proyecto.

---
+++ Model/Peliculas.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BlazorApp1.Model
+{
+    public class Peliculas
+    {
+        public int PeliculasId { get; set; }
+        public string Titulo { get; set; }
+        public string Director { get; set; }
+        public DateTime? Fecha { get; set; }
+    }
+}
